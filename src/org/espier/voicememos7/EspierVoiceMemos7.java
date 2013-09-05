61b76a83286dbf72aa1a4a2ffa984072c6b77d02
xueshuming <xueshuming@minigui.org>
2013-09-05 18:02:59 +0800

    init ios7 voicememos

---
+++ src/org/espier/voicememos7/EspierVoiceMemos7.java
@@ -0,0 +1,14 @@
+package org.espier.voicememos7;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EspierVoiceMemos7 extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
