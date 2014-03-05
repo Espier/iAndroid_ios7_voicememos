7476340dffca1200793004da64be1796d58664a9
Brad[C <=>
2014-03-05 20:34:03 +0800

    add get time perframe

--- src/org/espier/voicememos7/ui/VoiceMemoListAdapter.java
+++ src/org/espier/voicememos7/ui/VoiceMemoListAdapter.java
@@ -407,12 +407,12 @@ import java.util.List;
                             e.printStackTrace();
                             }
                     
-                    int[] framGains = mSoundFile.getFrameGains();
-                    int sampleRate = mSoundFile.getSampleRate();
-                    int numFrames = mSoundFile.getNumFrames();
-                    double []gainHeights = computeGainHeights();
-                    
-                    double time = (mSoundFile.getSamplesPerFrame() * numFrames)/sampleRate;
+//                    int[] framGains = mSoundFile.getFrameGains();
+//                    int sampleRate = mSoundFile.getSampleRate();
+//                    int numFrames = mSoundFile.getNumFrames();
+////                    double []gainHeights = computeGainHeights();
+//                    
+//                    double time = (mSoundFile.getSamplesPerFrame() * numFrames)/sampleRate;
                     setOnVoiceEditClicked(mSoundFile);
                 }
             });
@@ -478,87 +478,7 @@ import java.util.List;
             }
         }
 
-        private double[] computeGainHeights()
-        {
-            int numFrames = mSoundFile.getNumFrames();
-            int[] frameGains = mSoundFile.getFrameGains();
-            double[] smoothedGains = new double[numFrames];
-            if (numFrames == 1) {
-                smoothedGains[0] = frameGains[0];
-            } else if (numFrames == 2) {
-                smoothedGains[0] = frameGains[0];
-                smoothedGains[1] = frameGains[1];
-            } else if (numFrames > 2) {
-                smoothedGains[0] = (double)(
-                    (frameGains[0] / 2.0) +
-                    (frameGains[1] / 2.0));
-                for (int i = 1; i < numFrames - 1; i++) {
-                    smoothedGains[i] = (double)(
-                        (frameGains[i - 1] / 3.0) +
-                        (frameGains[i    ] / 3.0) +
-                        (frameGains[i + 1] / 3.0));
-                }
-                smoothedGains[numFrames - 1] = (double)(
-                    (frameGains[numFrames - 2] / 2.0) +
-                    (frameGains[numFrames - 1] / 2.0));
-            }
 
-            // Make sure the range is no more than 0 - 255
-            double maxGain = 1.0;
-            for (int i = 0; i < numFrames; i++) {
-                if (smoothedGains[i] > maxGain) {
-                    maxGain = smoothedGains[i];
-                }
-            }
-            double scaleFactor = 1.0;
-            if (maxGain > 255.0) {
-                scaleFactor = 255 / maxGain;
-            }        
-
-            // Build histogram of 256 bins and figure out the new scaled max
-            maxGain = 0;
-            int gainHist[] = new int[256];
-            for (int i = 0; i < numFrames; i++) {
-                int smoothedGain = (int)(smoothedGains[i] * scaleFactor);
-                if (smoothedGain < 0)
-                    smoothedGain = 0;
-                if (smoothedGain > 255)
-                    smoothedGain = 255;
-
-                if (smoothedGain > maxGain)
-                    maxGain = smoothedGain;
-
-                gainHist[smoothedGain]++;
-            }
-
-            // Re-calibrate the min to be 5%
-            double minGain = 0;
-            int sum = 0;
-            while (minGain < 255 && sum < numFrames / 20) {
-                sum += gainHist[(int)minGain];
-                minGain++;
-            }
-
-            // Re-calibrate the max to be 99%
-            sum = 0;
-            while (maxGain > 2 && sum < numFrames / 100) {
-                sum += gainHist[(int)maxGain];
-                maxGain--;
-            }
-
-            // Compute the heights
-            double[] heights = new double[numFrames];
-            double range = maxGain - minGain;
-            for (int i = 0; i < numFrames; i++) {
-                double value = (smoothedGains[i] * scaleFactor - minGain) / range;
-                if (value < 0.0)
-                    value = 0.0;
-                if (value > 1.0)
-                    value = 1.0;
-                heights[i] = value * value;
-            }
-            return heights;
-        }
         
         protected long refreshNow(ViewHolder view) {
             if (mCurrentMediaPlayer == null || mRecorder.getState() != Recorder.PLAYING_STATE) {
