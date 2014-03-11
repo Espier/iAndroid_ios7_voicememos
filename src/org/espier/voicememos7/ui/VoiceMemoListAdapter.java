ad118b17ab72c189259ef1af46e2d955fcd02e1a
xu duzhou <duzhou.xu@wbkit.com>
2014-03-11 19:51:00 +0800

    modify listview item ui

--- src/org/espier/voicememos7/ui/VoiceMemoListAdapter.java
+++ src/org/espier/voicememos7/ui/VoiceMemoListAdapter.java
@@ -218,25 +218,30 @@ class VoiceMemoListAdapter extends SimpleCursorAdapter {
                 ScalePx.scalePx(mContext, 13), 0, 0);
         holder.playControl.setLayoutParams(lpPlay);
 
-        LinearLayout.LayoutParams lpLeftTime = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT);
+        RelativeLayout.LayoutParams lpLeftTime = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
         lpLeftTime.setMargins(ScalePx.scalePx(mContext, 36),
                 0, 0, 0);
+        lpLeftTime.addRule(RelativeLayout.ALIGN_PARENT_LEFT,RelativeLayout.TRUE);
         holder.mCurrentTime.setLayoutParams(lpLeftTime);
-
-        LinearLayout.LayoutParams lpSeekBar = new LinearLayout.LayoutParams(
-                ScalePx.scalePx(mContext, 340),
-                LinearLayout.LayoutParams.WRAP_CONTENT);
+        
+        RelativeLayout.LayoutParams lpSeekBar = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        System.out.println("lay  width  "+lpSeekBar.width);
         lpSeekBar.setMargins(ScalePx.scalePx(mContext, 18),
                 ScalePx.scalePx(mContext, 13), 0, 0);
+        lpSeekBar.addRule(RelativeLayout.RIGHT_OF,R.id.current_positon);
+        lpSeekBar.addRule(RelativeLayout.LEFT_OF,R.id.current_remain);
         holder.bar.setLayoutParams(lpSeekBar);
 
-        LinearLayout.LayoutParams lpRightTime = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT);
+        RelativeLayout.LayoutParams lpRightTime = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
         lpRightTime.setMargins(ScalePx.scalePx(mContext, 18),
-                0, 0, 0);
+                0, ScalePx.scalePx(mContext, 33), 0);
+        lpRightTime.addRule(RelativeLayout.ALIGN_PARENT_RIGHT,RelativeLayout.TRUE);
         holder.mCurrentRemain.setLayoutParams(lpRightTime);
 
         LinearLayout.LayoutParams lpLine = new LinearLayout.LayoutParams(
